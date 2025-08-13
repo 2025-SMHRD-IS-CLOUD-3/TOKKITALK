a0e2d7e8ac9610881f9e76e19df8125d4e670f50
stella <a01072886128@gmail.com>
2025-08-13 10:56:12 +0900

    소민 수정

--- TokkiTalk/src/main/java/com/tokkitalk/controller/Login.java
+++ TokkiTalk/src/main/java/com/tokkitalk/controller/Login.java
@@ -7,18 +7,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class login
- */
+
 @WebServlet("/login")
 public class Login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+
 	}
 
 }
